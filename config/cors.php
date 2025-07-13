b2635e47576d464db4ae992f444c82fc65bc0744
kayllazahraa <168892439+kayllazahraa@users.noreply.github.com>
2025-07-13 11:51:58 +0700

    edit cors

--- config/cors.php
+++ config/cors.php
@@ -5,9 +5,9 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:5173',
-        'https://web-pbw-frontend.vercel.app'
+        'https://web-pbw-frontend.vercel.app/login'
     ],
-    'allowed_origins_patterns' => [''],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
